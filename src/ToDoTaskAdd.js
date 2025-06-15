1e4664ceb411f360028deecaf17161fb24bbd2c6
SkrillK <leonid15072004@gmail.com>
2025-06-15 20:52:51 +0300

    Reworked adding and updating processes.

--- src/ToDoTaskAdd.js
+++ src/ToDoTaskAdd.js
@@ -4,58 +4,142 @@ import { connect } from 'react-redux';
 import { todoAdd } from './actions';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGuitar } from '@fortawesome/free-solid-svg-icons';
+import { faGuitar, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+// Универсальный компонент для выбора и добавления опций
+class OptionAdder extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { newValue: '' };
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({ newValue: e.target.value });
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		if (this.state.newValue.trim()) {
+			this.props.onAdd(this.state.newValue);
+			this.setState({ newValue: '' });
+		}
+	}
+
+	render() {
+		return (
+			<div className="form-group">
+				<label>{this.props.label}</label>
+				<select
+					value={this.props.selectedValue}
+					onChange={this.props.onChange}
+					className="form-control mb-2"
+					required
+				>
+					<option value="" disabled>Select {this.props.label.toLowerCase()}</option>
+					{this.props.options.map((option, index) => (
+						<option key={index} value={option}>{option}</option>
+					))}
+				</select>
+
+				<div className="input-group mb-3">
+					<input
+						type="text"
+						value={this.state.newValue}
+						onChange={this.onChange}
+						placeholder={`Add new ${this.props.label.toLowerCase()}`}
+						className="form-control"
+					/>
+					<div className="input-group-append">
+						<button type="submit" className="btn btn-outline-secondary" onClick={this.onSubmit}>
+							<FontAwesomeIcon icon={faPlus} />
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 class ToDoTaskAddInner extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state= {
+		this.state = {
 			brand: '',
 			name: '',
 			color: '',
 			form: '',
-			strings: ''
-		}
+			strings: '',
+			brands: ['Fender', 'Gibson', 'Ibanez', 'Jackson', 'Schecter', 'Luxars', 'ESP'],
+			colors: ['Red', 'Black', 'White', 'Blue', 'Sunburst'],
+			forms: ['Explorer', 'Stratocaster', 'Superstrat', 'Les Paul', 'Headless'],
+			stringsOptions: ['6', '7', '8']
+		};
+
 		this.onBrandChange = this.onBrandChange.bind(this);
 		this.onNameChange = this.onNameChange.bind(this);
 		this.onColorChange = this.onColorChange.bind(this);
 		this.onFormChange = this.onFormChange.bind(this);
 		this.onStringsChange = this.onStringsChange.bind(this);
 		this.onAddFormSubmit = this.onAddFormSubmit.bind(this);
+		this.addBrand = this.addBrand.bind(this);
+		this.addColor = this.addColor.bind(this);
+		this.addForm = this.addForm.bind(this);
+		this.addStrings = this.addStrings.bind(this);
 	}
+
+	addBrand(newBrand) {
+		this.setState(prevState => ({
+			brands: [...prevState.brands, newBrand],
+			brand: newBrand
+		}));
+	}
+
+	addColor(newColor) {
+		this.setState(prevState => ({
+			colors: [...prevState.colors, newColor],
+			color: newColor
+		}));
+	}
+
+	addForm(newForm) {
+		this.setState(prevState => ({
+			forms: [...prevState.forms, newForm],
+			form: newForm
+		}));
+	}
+
+	addStrings(newStrings) {
+		this.setState(prevState => ({
+			stringsOptions: [...prevState.stringsOptions, newStrings],
+			strings: newStrings
+		}));
+	}
+
 	onBrandChange(e) {
-		e.preventDefault();
-		this.setState({
-			brand: e.target.value
-		});
+		this.setState({ brand: e.target.value });
 	}
+
 	onNameChange(e) {
-		e.preventDefault();
-		this.setState({
-			name: e.target.value
-		});
+		this.setState({ name: e.target.value });
 	}
+
 	onColorChange(e) {
-		e.preventDefault();
-		this.setState({
-			color: e.target.value
-		});
+		this.setState({ color: e.target.value });
 	}
+
 	onFormChange(e) {
-		e.preventDefault();
-		this.setState({
-			form: e.target.value
-		});
+		this.setState({ form: e.target.value });
 	}
+
 	onStringsChange(e) {
-		e.preventDefault();
-		this.setState({
-			strings: e.target.value
-		});
+		this.setState({ strings: e.target.value });
 	}
-	onAddFormSubmit(e){
+
+	onAddFormSubmit(e) {
 		e.preventDefault();
-		fetch('tasks',{
+		fetch('tasks', {
 			method: 'POST',
 			body: JSON.stringify({
 				brand: this.state.brand,
@@ -69,11 +153,12 @@ class ToDoTaskAddInner extends React.Component {
 			}
 		}).then((res) => {
 			return res.json();
-		}).then((data) =>{
+		}).then((data) => {
 			this.props.dispatch(todoAdd(data._id, data.brand, data.name, data.color, data.form, data.strings));
 			this.props.history('/');
-		}); 
+		});
 	}
+
 	render() {
 		return (
 			<div className="card-hover-shadow-2x mb-3 card">
@@ -82,68 +167,75 @@ class ToDoTaskAddInner extends React.Component {
 						<FontAwesomeIcon icon={faGuitar} />&nbsp;Add Guitar
 					</div>
 				</div>
-				<form onSubmit={this.onAddFormSubmit}>
-				<div className="widget-content">
-					<div className="widget-content-wrapper">
-						<select
-							name="brand"
-							value={this.state.brand}
-							onChange={this.onBrandChange}
-							className="form-control"
-							required>
-							<option value="" disabled>Brand</option>
-							<option value="Fender">Fender</option>
-							<option value="Gibson">Gibson</option>
-							<option value="Ibanez">Ibanez</option>
-							<option value="Jackson">Jackson</option>
-							<option value="Schecter">Schecter</option>
-							<option value="Luxars">Luxars</option>
-							<option value="ESP">ESP</option>
-						</select>
 
-						<input type="text" value={this.state.name} onChange={this.onNameChange} placeholder="Model" className="form-control" />
-						<input type="text" value={this.state.color} onChange={this.onColorChange} placeholder="Color" className="form-control" />
-						<select
-							name="form"
-							value={this.state.form}
-							onChange={this.onFormChange}
-							className="form-control"
-							required>
-							<option value="" disabled>Form</option>
-							<option value="Explorer">Explorer</option>
-							<option value="Stratocaster">Stratocaster</option>
-							<option value="Superstrat">Superstrat</option>
-							<option value="Les Paul">Les Paul</option>
-							<option value="Headless">Headless</option>
-						</select>
-						<select
-							name="strings"
-							value={this.state.strings}
-							onChange={this.onStringsChange}
-							className="form-control"
-							required>
-							<option value="" disabled>Strings</option>
-							<option value="6">6</option>
-							<option value="7">7</option>
-							<option value="8">8</option>
-						</select>
-					</div>
-				</div>
-				</form> 
-				
-				<div className="d-block text-right card-footer" >
+				<div className="card-body">
 					<form onSubmit={this.onAddFormSubmit}>
-						<input type="submit" value="Add" className="btn btn-primary" />
+						<div className="form-container">
+							{/* Бренд */}
+							<OptionAdder
+								selectedValue={this.state.brand}
+								onChange={this.onBrandChange}
+								options={this.state.brands}
+								onAdd={this.addBrand}
+								label="Brand"
+							/>
+
+							{/* Модель */}
+							<div className="form-group">
+								<label>Model</label>
+								<input
+									type="text"
+									value={this.state.name}
+									onChange={this.onNameChange}
+									placeholder="Enter model name"
+									className="form-control"
+									required
+								/>
+							</div>
+
+							{/* Цвет */}
+							<OptionAdder
+								selectedValue={this.state.color}
+								onChange={this.onColorChange}
+								options={this.state.colors}
+								onAdd={this.addColor}
+								label="Color"
+							/>
+
+							{/* Форма */}
+							<OptionAdder
+								selectedValue={this.state.form}
+								onChange={this.onFormChange}
+								options={this.state.forms}
+								onAdd={this.addForm}
+								label="Form"
+							/>
+
+							{/* Струны */}
+							<OptionAdder
+								selectedValue={this.state.strings}
+								onChange={this.onStringsChange}
+								options={this.state.stringsOptions}
+								onAdd={this.addStrings}
+								label="Strings"
+							/>
+						</div>
+
+						<div className="d-block text-right card-footer">
+							<button type="submit" className="btn btn-primary mr-2">Add Guitar</button>
+							<NavLink to='/' className="btn btn-secondary">Back to list</NavLink>
+						</div>
 					</form>
-					<NavLink to='/' className="btn btn-primary"> Back to list </NavLink>
 				</div>
 			</div>
-		)
+		);
 	}
 }
+
 const ToDoTaskAdd = (props) => {
 	return (
 		<ToDoTaskAddInner {...props} history={useNavigate()} />
-	)
-}
+	);
+};
+
 export default connect()(ToDoTaskAdd);
--- src/ToDoTaskUpdate.js
+++ src/ToDoTaskUpdate.js
@@ -1,20 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { todoUpdateState } from './actions';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGuitar } from '@fortawesome/free-solid-svg-icons';
+import { faGuitar, faPlus } from '@fortawesome/free-solid-svg-icons';
 
+// Универсальный компонент для выбора и добавления опций (такой же как в первом файле)
+class OptionAdder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { newValue: '' };
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
 
-const UpdateBook = () => {
+    onChange(e) {
+        this.setState({ newValue: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        if (this.state.newValue.trim()) {
+            this.props.onAdd(this.state.newValue);
+            this.setState({ newValue: '' });
+        }
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <label>{this.props.label}</label>
+                <select
+                    name={this.props.name}
+                    value={this.props.selectedValue}
+                    onChange={this.props.onChange}
+                    className="form-control mb-2"
+                    required
+                >
+                    <option value="" disabled>Select {this.props.label.toLowerCase()}</option>
+                    {this.props.options.map((option, index) => (
+                        <option key={index} value={option}>{option}</option>
+                    ))}
+                </select>
+
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        value={this.state.newValue}
+                        onChange={this.onChange}
+                        placeholder={`Add new ${this.props.label.toLowerCase()}`}
+                        className="form-control"
+                    />
+                    <div className="input-group-append">
+                        <button type="submit" className="btn btn-outline-secondary" onClick={this.onSubmit}>
+                            <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const UpdateGuitar = ({ dispatch }) => {
     const { id } = useParams();
     const navigate = useNavigate();
     const [task, setTask] = useState({
         brand: '',
         name: '',
-		color: '',
-		form: '',
-		strings: ''
+        color: '',
+        form: '',
+        strings: ''
+    });
+    
+    const [options, setOptions] = useState({
+        brands: ['Fender', 'Gibson', 'Ibanez', 'Jackson', 'Schecter', 'Luxars', 'ESP'],
+        colors: ['Red', 'Black', 'White', 'Blue', 'Sunburst'],
+        forms: ['Explorer', 'Stratocaster', 'Superstrat', 'Les Paul', 'Headless'],
+        stringsOptions: ['6', '7', '8']
     });
 
     useEffect(() => {
@@ -33,17 +97,50 @@ const UpdateBook = () => {
         }));
     };
 
+    const addBrand = (newBrand) => {
+        setOptions(prev => ({
+            ...prev,
+            brands: [...prev.brands, newBrand]
+        }));
+        setTask(prev => ({ ...prev, brand: newBrand }));
+    };
+
+    const addColor = (newColor) => {
+        setOptions(prev => ({
+            ...prev,
+            colors: [...prev.colors, newColor]
+        }));
+        setTask(prev => ({ ...prev, color: newColor }));
+    };
+
+    const addForm = (newForm) => {
+        setOptions(prev => ({
+            ...prev,
+            forms: [...prev.forms, newForm]
+        }));
+        setTask(prev => ({ ...prev, form: newForm }));
+    };
+
+    const addStrings = (newStrings) => {
+        setOptions(prev => ({
+            ...prev,
+            stringsOptions: [...prev.stringsOptions, newStrings]
+        }));
+        setTask(prev => ({ ...prev, strings: newStrings }));
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
         const updatedData = {
             _id: task._id,
-			brand: task.brand,
-			name: task.name,
-			color: task.color,
-			form: task.form,
-			strings: task.strings
-        }
+            brand: task.brand,
+            name: task.name,
+            color: task.color,
+            form: task.form,
+            strings: task.strings
+        };
+
         fetch(`/update/${id}`, {
             method: 'PATCH',
             headers: {
@@ -51,110 +148,87 @@ const UpdateBook = () => {
             },
             body: JSON.stringify(updatedData),
         })
-            .then(res => {
-                if (res.ok) {
-                    navigate('/', { state: { forceRefresh: true } });
-					window.location.reload();
-                }
-            });
+        .then(res => res.json())
+        .then(data => {
+            dispatch(todoUpdateState(data._id, data.brand, data.name, data.color, data.form, data.strings));
+            navigate('/');
+        });
     };
 
     return (
-		<div className="card-hover-shadow-2x mb-3 card">
-			<div className="card-header-tab card-header">
-				<div className="card-header-title font-size-lg text-capitalize font-weight-normal">
-					<FontAwesomeIcon icon={faGuitar} />&nbsp;Update Guitar
-				</div>
-			</div>
-			<div className="widget-content">
-				<div className="widget-content-wrapper">
-					<form onSubmit={handleSubmit}>
-						<div>
-							<select
-								name="brand"
-								value={task.brand}
-								onChange={handleChange}
-								className="form-control"
-								required>
-								<option value="" disabled>Brand</option>
-								<option value="Fender">Fender</option>
-								<option value="Gibson">Gibson</option>
-								<option value="Ibanez">Ibanez</option>
-								<option value="Jackson">Jackson</option>
-								<option value="Schecter">Schecter</option>
-								<option value="Luxars">Luxars</option>
-								<option value="ESP">ESP</option>
-							</select>
-						</div>
-						<div>
-							<input
-								type="text"
-								name="name"
-								value={task.name}
-								onChange={handleChange}
-								required
-							placeholder="Model" className="form-control"/>
-						</div>
-						<div>
-							<input
-								name="color"
-								value={task.color}
-								onChange={handleChange}
-								required
-							placeholder="Color" className="form-control"/>
-						</div>
-						<div>
-							<select
-								name="form"
-								value={task.form}
-								onChange={handleChange}
-								className="form-control"
-								required>
-								<option value="" disabled>Form</option>
-								<option value="Explorer">Explorer</option>
-								<option value="Stratocaster">Stratocaster</option>
-								<option value="Superstrat">Superstrat</option>
-								<option value="Les Paul">Les Paul</option>
-								<option value="Headless">Headless</option>
-							</select>
-						</div>
-						<div>
-							<select
-								name="strings"
-								value={task.strings}
-								onChange={handleChange}
-								className="form-control"
-								required>
-								<option value="" disabled>Strings</option>
-								<option value="6">6</option>
-								<option value="7">7</option>
-								<option value="8">8</option>
-							</select>	
-						</div>
-							
-					</form>
-				</div>
-			</div>
-				<div className="d-block text-left card-footer">
-					<form onSubmit={handleSubmit}>
-						<div>
-							<button 
-								style={{ 
-									backgroundColor: '#343a40', 
-									border: 'none', 
-									color: '#ffffff', 
-									borderRadius: '5px', 
-									padding: '5px 10px' 
-								}}
-								type="submit">
-									Apply
-							</button>
-						</div>
-					</form>
-					<NavLink to='/' className="btn btn-primary" >Back to list</NavLink>
-				</div>
-		</div>
-	);
+        <div className="card-hover-shadow-2x mb-3 card">
+            <div className="card-header-tab card-header">
+                <div className="card-header-title font-size-lg text-capitalize font-weight-normal">
+                    <FontAwesomeIcon icon={faGuitar} />&nbsp;Update Guitar
+                </div>
+            </div>
+
+            <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-container">
+                        {/* Бренд */}
+                        <OptionAdder
+                            name="brand"
+                            selectedValue={task.brand}
+                            onChange={handleChange}
+                            options={options.brands}
+                            onAdd={addBrand}
+                            label="Brand"
+                        />
+
+                        {/* Модель */}
+                        <div className="form-group">
+                            <label>Model</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={task.name}
+                                onChange={handleChange}
+                                placeholder="Enter model name"
+                                className="form-control"
+                                required
+                            />
+                        </div>
+
+                        {/* Цвет */}
+                        <OptionAdder
+                            name="color"
+                            selectedValue={task.color}
+                            onChange={handleChange}
+                            options={options.colors}
+                            onAdd={addColor}
+                            label="Color"
+                        />
+
+                        {/* Форма */}
+                        <OptionAdder
+                            name="form"
+                            selectedValue={task.form}
+                            onChange={handleChange}
+                            options={options.forms}
+                            onAdd={addForm}
+                            label="Form"
+                        />
+
+                        {/* Струны */}
+                        <OptionAdder
+                            name="strings"
+                            selectedValue={task.strings}
+                            onChange={handleChange}
+                            options={options.stringsOptions}
+                            onAdd={addStrings}
+                            label="Strings"
+                        />
+                    </div>
+
+                    <div className="d-block text-right card-footer">
+                        <button type="submit" className="btn btn-primary mr-2">Update Guitar</button>
+                        <NavLink to='/' className="btn btn-secondary">Back to list</NavLink>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
-export default connect()(UpdateBook);
+export default connect()(UpdateGuitar);
